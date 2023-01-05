58eca063e62d9a9a72e8370a3d5a5a32159a297b
Federico Castelli <fc@oracleofde.fi>
2023-01-05 16:11:03 +0100

    Update npmAccess flag to restricted

--- .projenrc.js
+++ .projenrc.js
@@ -1,5 +1,6 @@
 const { awscdk } = require('projen');
 const { Stability } = require('projen/lib/cdk');
+const { NpmAccess } = require('projen/lib/javascript');
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'ODF Data',
   authorUrl: 'https://odfdata.com',
@@ -10,7 +11,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   repositoryUrl: 'git@github.com:odfdata/blockchain-listener.git',
   authorOrganization: true,
   releaseToNpm: true,
-  npmAccess: 'public',
+  npmAccess: NpmAccess.RESTRICTED,
   description: 'The AWS Constructor to create a Blockchain Listener using AWS Fargate and Event Bridge',
   stability: Stability.EXPERIMENTAL,
   gitignore: ['.idea'],
