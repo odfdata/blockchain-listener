3c2948775d6beef81661a7f52183b56f8848a15e
Federico Castelli <fc@oracleofde.fi>
2023-01-05 15:05:20 +0100

    Publish package under organization

--- .projenrc.js
+++ .projenrc.js
@@ -1,13 +1,13 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'Federico Castelli',
-  authorAddress: 'fc@oracleofde.fi',
+  author: 'ODF Data',
+  authorAddress: 'it@odfdata.com',
   cdkVersion: '2.54.0',
   defaultReleaseBranch: 'master',
-  name: 'blockchain-listener',
+  name: '@odfdata/blockchain-listener',
   repositoryUrl: 'git@github.com:odfdata/blockchain-listener.git',
   releaseToNpm: true,
-  packageName: 'blockchain-listener',
+  packageName: '@odfdata/blockchain-listener',
   description: 'The AWS Constructor to create a Blockchain Listener using AWS Fargate and Event Bridge',
   stability: 'experimental',
   gitignore: ['.idea'],
